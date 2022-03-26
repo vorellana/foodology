37652378360ab5c8e7015ff7978aa4d5a05b8b84
victor <victor.orellana@2bak.cl>
2022-03-25 19:44:48 -0500

    add error handle

--- src/controllers/location.controller.js
+++ src/controllers/location.controller.js
@@ -2,8 +2,13 @@ const exp = {}
 const { getLocation } = require('../services/location.service')
 
 exp.getLocation = async (req, res) => {
-    let location = await getLocation();
-    res.status(200).json({location});
+    let { token } = req.body;
+    try {
+        let location = await getLocation(token);
+        res.status(200).json({location});
+    } catch (error) {
+        res.status(400).json({error: error.message, tokenError: error.isAxiosError});
+    }
 }
 
 module.exports = exp;
--- src/services/location.service.js
+++ src/services/location.service.js
@@ -1,9 +1,9 @@
 const exp = {}
 const { getLocation } = require('../utils/location')
 
-exp.getLocation = async () => {
+exp.getLocation = async (token) => {
     console.log('getLocation service');
-    const location = await getLocation();
+    const location = await getLocation(token);
     return location;
 }
 
