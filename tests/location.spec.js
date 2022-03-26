9edb1b01df99a7f6bc8c70145443a0da214f08f0
victor <victor.orellana@2bak.cl>
2022-03-25 20:39:21 -0500

    add unit test

--- tests/location.spec.js
+++ tests/location.spec.js
@@ -1,68 +1,19 @@
 const request = require('supertest')
 const app = require('../src/app')
-const { payload1, payload2, payload3 } = require('./test-data')
-const urlBase = '/v1/b-trees';
-const urlHeight = urlBase + '/height';
-const urlNeighbors = urlBase + '/neighbors';
-const urlBfs = urlBase + '/bfs';
+const urlBase = '/api-foodology';
+const urlLocation = urlBase + '/location';
 
-describe('POST ' + urlHeight, () => {
-    test('The input payload must have property toTree', () => {
-        expect(payload1).toHaveProperty('toTree');
-    })
+describe('GET ' + urlLocation, () => {
     test('should respond with a 200 status code', async () => {
-        const response = await request(app).post(urlHeight).send(payload1);
+        const response = await request(app).get(urlLocation);
         expect(response.statusCode).toBe(200);
     })
     test('should respond with a json', async () => {
-        const response = await request(app).post(urlHeight).send(payload1);
+        const response = await request(app).get(urlLocation);
         expect(response.body).toBeInstanceOf(Object);
     })    
     test('should respond with a Content-type: application/json header', async () => {
-        const response = await request(app).post(urlHeight).send(payload1);
-        expect(response.headers["content-type"]).toEqual(
-            expect.stringContaining("json")
-        );
-    })
-})
-
-describe('POST ' + urlNeighbors, () => {
-    test('The input payload must have property toTree', () => {
-        expect(payload2).toHaveProperty('toTree');
-    })
-    test('The input payload must have property node', () => {
-        expect(payload2).toHaveProperty('node');
-    })
-    test('should respond with a 200 status code', async () => {
-        const response = await request(app).post(urlNeighbors).send(payload2);
-        expect(response.statusCode).toBe(200);
-    })
-    test('should respond with a json', async () => {
-        const response = await request(app).post(urlNeighbors).send(payload2);
-        expect(response.body).toBeInstanceOf(Object);
-    })    
-    test('should respond with a Content-type: application/json header', async () => {
-        const response = await request(app).post(urlNeighbors).send(payload2);
-        expect(response.headers["content-type"]).toEqual(
-            expect.stringContaining("json")
-        );
-    })
-})
-
-describe('POST ' + urlBfs, () => {
-    test('The input payload must have property toTree', () => {
-        expect(payload3).toHaveProperty('toTree');
-    })
-    test('should respond with a 200 status code', async () => {
-        const response = await request(app).post(urlBfs).send(payload3);
-        expect(response.statusCode).toBe(200);
-    })
-    test('should respond with a json', async () => {
-        const response = await request(app).post(urlBfs).send(payload3);
-        expect(response.body).toBeInstanceOf(Object);
-    })    
-    test('should respond with a Content-type: application/json header', async () => {
-        const response = await request(app).post(urlBfs).send(payload3);
+        const response = await request(app).get(urlLocation);
         expect(response.headers["content-type"]).toEqual(
             expect.stringContaining("json")
         );
